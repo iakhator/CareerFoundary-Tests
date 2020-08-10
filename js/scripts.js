828114d6337bec84da635f9b0bef3cbd655078b0
Akhator Itua <iakhator@gmail.com>
2020-08-10 13:21:32 +0100

    feat: create pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-alert('Hello world');
-let favouriteFood = 'pizza';
-document.write(favouriteFood);
+let pokemonList = [
+  {
+  name: "Pikachu",
+  height: 5,
+  type: ['lime', 'lemon']
+  },
+  {
+    name: "Bulbasaur",
+    height: 7,
+    type: ['grass', 'poison']
+  },
+  {
+  name: "Blastoise",
+  height: 1.6,
+  type: ['water']
+  },
+  {
+    name: "Beedrill",
+    height: 1,
+    type: ['bug', 'posion']
+  },
+  {
+    name: "Warturtle",
+    height: 1,
+    type: ['water']
+  }
+];
