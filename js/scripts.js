d6927b64899c6b4562783100886cfa6145139741
Akhator Itua <iakhator@gmail.com>
2020-08-12 16:19:15 +0100

    feat: practise excercise

--- js/scripts.js
+++ js/scripts.js
@@ -45,25 +45,38 @@ let pokemonRepository = (function () {
     return pokemonList.filter((pokemon) => pokemon.name == name)
   }
 
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-btn');
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+  }
+
   return  {
     add: add,
     getAll: getAll,
-    getPokemon: getPokemon
+    getPokemon: getPokemon,
+    addListItem: addListItem,
   }
 })();
 
 //loop through pokemonlist to display pokemon
 pokemonRepository.getAll().forEach(pokemon => {
+  
+  pokemonRepository.addListItem(pokemon)
   //declare result to hold pokemon value
-  let result = `${pokemon.name} (${pokemon.height})`;
-  //check if height is greater than 7;
-  if (pokemon.height >= 7) {
-    result = `<div>${result} - Wow, that's big!</div>`
-  } else {
-    result = `<div>${result}</div>`
-  }
-  //display the result
-  document.write(result)
+  // let result = `${pokemon.name} (${pokemon.height})`;
+  // //check if height is greater than 7;
+  // if (pokemon.height >= 7) {
+  //   result = `<div>${result} - Wow, that's big!</div>`
+  // } else {
+  //   result = `<div>${result}</div>`
+  // }
+  // //display the result
+  // document.write(result)
 }) 
 
 window.repo = pokemonRepository;
